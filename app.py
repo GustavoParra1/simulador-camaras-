243fa9c8a2e83797c54484e08155f88c6f851cf1
GustavoParra1 <108834013+GustavoParra1@users.noreply.github.com>
2025-06-13 10:58:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if direccion:
                 ).add_to(cluster)
 
             # Mostrar el mapa
-            st_folium(mapa, width=700, height=500)
+            st_folium(mapa, width=1200, height=600)
             st.write("Columnas en CSV:", df_camaras.columns.tolist())
 
 
